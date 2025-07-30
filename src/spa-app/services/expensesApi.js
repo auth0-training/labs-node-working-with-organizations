f6e94ac700557561254d3c99aed09da3c4d20f01
Sara Kazemi <sara.kazemi@okta.com>
2025-07-29 22:24:28 -0700

    Update expensesApi.js

--- src/spa-app/services/expensesApi.js
+++ src/spa-app/services/expensesApi.js
@@ -1,27 +1,35 @@
 const expensesApi = {
   getTotals: async () => {
     try {
-      return {"total":144,"count":2};
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await fetch(`${window.env.API_URL}/total`, options);
+      const json = await res.json();
+      return json;
     } catch (err) {
       console.log("Error getting total", err);
     }
   },
   getReports: async () => {
     try {
-      // 👇 Replace this with call to API 👇
-      return [
-        {
-          date: new Date(),
-          description: "Pizza for a Coding Dojo session.",
-          value: 102,
-        },
-        {
-          date: new Date(),
-          description: "Coffee for a Coding Dojo session.",
-          value: 42,
-        },
-      ];
-      // 👆 Replace this with call to API 👆
+
+      const token = await window.auth0Client.getTokenSilently();
+      const options = {
+          method: "GET",
+          headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          },
+      };
+      const res = await fetch(`${window.env.API_URL}/reports`, options);
+      const json = await res.json();
+      return json;
+
     } catch (err) {
       console.log("Error getting reports", err);
     }
